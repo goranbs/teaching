4aafdacbf5ad2e0650e234d0ad85ddb7f7be00be
Gøran Brekke Svaland <goranbs@student.matnat.uio.no>
2014-02-28 16:51:57 +0100

    working

--- FYS-MEK1110-mechanics/stick_slip.py
+++ FYS-MEK1110-mechanics/stick_slip.py
@@ -22,12 +22,12 @@ from scitools.std import *
 
 m = 0.1     # [kg] weight of box
 g = 9.81    # [m/s^2] acceleration of gravity
-b = 0.0     # [m] equilibrium length F = 0
+b = 0.1     # [m] equilibrium length F = 0
 v0 = 0.0    # [m/s] initial velocity
 u = 0.1     # [m/s] constant velocity of spring attachment
 k = 100     # [N/m] spring coefficient
-my_s = 0.6  # static friction
-my_d = 0.3  # dynamic friction
+my_s = 0.0  # static friction
+my_d = 0.0  # dynamic friction
 N = m*g     # [N] Normal force
 omega = sqrt(k/m) # [1/s] freqency
 
@@ -36,7 +36,7 @@ tmax = 2.0  # [s] stop time of simulation
 n = 10000    # Number of timesteps
 dt = (tmax-t0)/float(n) # timestep
 print 'Timestep dt=%0.4f' %dt
-x0 = 0
+x0 = 0.0
 
 a = zeros(n)
 v = zeros(n)
@@ -49,40 +49,33 @@ t = zeros(n)
 v[0] = v0
 x[0] = x0
 
-##############################################
-# functions:
-
-def friction(F):
-    if F >= my_s*N:
-        #print 'my_d'
-        return my_d*N
-    else:
-        #print 'my_s'
-        return my_s*N
-
-def spring(x,t):
-    return (k/m)*(Xb(t) - x - b)
-
-def Xb(t):
-    return u*t
-
 ################################################
 # the for-loop        
 
 for i in range(n-1):
-    F[i] = spring(x[i],t[i])
-    R[i] = friction(F[i])
+    xb[i] = b + u*t[i]
+    F[i] = k*(xb[i]-x[i]-b)
+    if (v[i] == 0):
+        R[i] = F[i]
+        if (abs(R[i]) >= my_s*N):
+            R[i] = my_d*N
+    else:
+        R[i] = my_d*N
+
     a[i] = (F[i] - R[i])/m
     v[i+1] = v[i] + a[i]*dt
-    xb[i] = Xb(t[i])
-    x[i+1] = x[i] + v[i]*dt + xb[i]
+    if ((v[i+1] < 0 and v[i] > 0) or (v[i+1] > 0 and v[i] < 0)):
+        print "if statement is true, t=%.3f" % t[i]         
+        v[i+1] = 0
+    
+    x[i+1] = x[i] + v[i+1]*dt
     t[i+1] = t[i] + dt
 
 
 a[-1] = a[-2]
 xb[-1] = xb[-2]
 
-x_exact = Xb(t) - (u/omega)*sin(omega*t)
+x_exact = u*t - (u/omega)*sin(omega*t)
 
 ###################################################
 #Plotting
@@ -91,7 +84,7 @@ import matplotlib.pyplot as plt
 plt.figure()
 plt.plot(t[:],F[:],'r-o')
 plt.hold(True)
-plt.plot(t[:],R[:],'b-*')
+plt.plot(t[:],-R[:],'b-*')
 plt.legend(('F(t)', 'R(t)'))
 plt.title('Forces. dt=%.4f' % dt )
 plt.xlabel('time [s]')
@@ -109,18 +102,13 @@ plt.plot(t[:],v[:])
 plt.ylabel('Velocity [m/s]')
 plt.legend('v(t)')
 plt.subplot(3,1,3)
-plt.plot(t[:],x[:])
+plt.plot(t[:],x[:], 'b-')
+plt.hold(True)
+plt.plot(t,x_exact, 'r-')
 plt.ylabel('position [m]')
-plt.legend('x(t)')
+plt.legend(('numerical','exact'))
 plt.xlabel('time [s]')
-
-
-#plt.figure()
-#plt.plot(t,xb)
-#plt.ylabel('position [m]')
-#lt.xlabel('time [s]')
-#lt.legend('xb(t)')
-#plt.title('position of spring')
+plt.hold(False)
 
 plt.figure()
 plt.plot(t,x_exact)
@@ -128,8 +116,8 @@ plt.hold(True)
 plt.plot(t,x)
 plt.ylabel('position [m]')
 plt.xlabel('time [s]')
-plt.legend(('x_{exact}(t)', 'x(t)'))
+plt.legend(('exact', 'numerical'))
 plt.title('position of spring. u=%.2f' % u)
-
+plt.hold(False)
 
 plt.show(True)
