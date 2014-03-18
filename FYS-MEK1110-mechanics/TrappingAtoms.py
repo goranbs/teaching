b61cfd85decf1f25d3567e9b78b7426a0cd2b5bc
Gøran Brekke Svaland <goranbs@student.matnat.uio.no>
2014-03-18 21:56:08 +0100

    added exercisek)

--- FYS-MEK1110-mechanics/TrappingAtoms.py
+++ FYS-MEK1110-mechanics/TrappingAtoms.py
@@ -18,7 +18,9 @@ x = zeros(n)
 v = zeros(n)
 
 # Initial Conditions
+
 #x[0], v[0] = -5.0, 8.0
+#x[0], v[0] = -5.0, 10.0
 #x[0], v[0] = 0, 8.0
 x[0], v[0] = 0, sqrt(4*U0/m)
 
@@ -47,3 +49,37 @@ plot(t,v)
 title('velocity of particle')
 xlabel('time')
 ylabel('velocity')
+
+
+###########################################################
+# Find largest initial velocity for the atom not to escape:
+
+noEscape = []
+x1 = zeros(n)
+v1 = zeros(n)
+x1[0] = -5
+# Need to have a closer look at this part:
+for v0 in linspace(8,10,10001):
+    v1[0] = v0
+    for i in xrange(n-1):
+        # Magnetic force M
+        # Photon force F
+        if abs(x1[i]) >= x0:
+            M,F = 0, 0
+            
+        else:
+            M = -U0/x0*cmp(x1[i],0)
+            F = -alpha*v1[i]
+                
+        a = (M+F)/m
+        v1[i+1] = v1[i] + a*dt
+        x1[i+1] = x1[i] + v1[i+1]*dt
+                
+        if x1[i+1] > x0:
+            break
+        
+    noEscape.append(v0)
+     
+        
+print 'For initial position x[0]=%.2f . \n Largest initial velocity that the atom can have \n and still not escape is v0=%.3f ' % (x1[0],noEscape[-1])
+
