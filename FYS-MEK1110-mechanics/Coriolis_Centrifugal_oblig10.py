0d882e35ea7b9c2eea8be3e81ba344a628ef2cc1
goranbs <goranbs@student.matnat.uio.no>
2014-05-13 13:54:34 +0200

    update on oblig10

--- FYS-MEK1110-mechanics/Coriolis_Centrifugal_oblig10.py
+++ FYS-MEK1110-mechanics/Coriolis_Centrifugal_oblig10.py
@@ -13,8 +13,8 @@ from math import cos,sin,pi,sqrt
 import numpy as np
 
 
-m = 0.02                           # mass bullet
-Rb = 2.8*10**(-3)                  # radius of bullet
+m = 0.002                          # mass bullet
+Rb = 0.0028                        # radius of bullet
 A = pi*Rb**2                       # cross-section of bullet
 rho = 1.225                        # [kg/m**3] density of air
 Cv = 0.3                           # assumed drag coeff of bullet
@@ -49,15 +49,17 @@ max_it = 0
 
 for t in range(n-1):
    #                                  (vz*cos(phi)   - vy*sin(phi)) i  +  vx*sin(phi) j  +  vx*cos(phi) k 
-    Fc[t,:] = -2*omega*np.array([(v[t,2]*cos(phi) - v[t,1]*sin(phi)) , v[t,0]*sin(phi) , v[t,0]*cos(phi) ])    
-    Fs[t,:] = -(omega**2)*np.array([(-R[t,0]*cos(phi)**2 - sin(phi)*((R[t,2])*cos(phi) - R[t,1]*sin(phi)) ) , (sin(phi)*((R[t,2])*cos(phi) - r[t,1]*sin(phi))) , cos(phi)*(R[t,1]*sin(phi) - (R[t,2])*cos(phi)) ])    
-    #Fs[t,:] = -(omega**2*Re)*np.array([0,sin(phi)*cos(phi), cos(phi)**2])   
-    Fd[t,:] = -D*abs(sqrt(v[t,0]*v[t,0] + v[t,1]*v[t,1] + v[t,2]*v[t,2]))*v[t,:] 
-    G = np.array([0,0,-g])
+    Fc[t,:] = -m*2*omega*np.array([(v[t,2]*cos(phi) - v[t,1]*sin(phi)) , v[t,0]*sin(phi) , v[t,0]*cos(phi) ])    
+    Fs[t,:] = -m*(omega**2)*np.array([(-R[t,0]*cos(phi)**2 - sin(phi)*((R[t,2])*cos(phi) - R[t,1]*sin(phi)) ) , (sin(phi)*((R[t,2])*cos(phi) - r[t,1]*sin(phi))) , cos(phi)*(R[t,1]*sin(phi) - (R[t,2])*cos(phi)) ])    
+    #Fs[t,:] = -(m*omega**2*Re)*np.array([0,sin(phi)*cos(phi), cos(phi)**2])
+    vel = abs(sqrt(v[t,0]*v[t,0] + v[t,1]*v[t,1] + v[t,2]*v[t,2]))
+    Fd[t,:] = -m*D*(vel**2)*(v[t,:]/vel)
+    #Fd[t,:] = -m*D*(vel**2)*np.sign(v[t,:]) 
+    G = np.array([0,0,-m*g])
     
-    #a[t,:] = (Fc[t,:] + G + Fs[t,:] + Fd[t,:]) # including centrifugal & drag
-    #a[t,:] = (Fc[t,:] + G + Fs[t,:]) # including centrifugal 
-    a[t,:] = (Fc[t,:] + G) # without centrifugal 
+    a[t,:] = (Fc[t,:] + G + Fs[t,:] + Fd[t,:])/m # including centrifugal & drag
+    #a[t,:] = (Fc[t,:] + G + Fs[t,:])/m # including centrifugal 
+    #a[t,:] = (Fc[t,:] + G)/m # without centrifugal 
         
     v[t+1,:] = v[t,:] + a[t,:]*dt
     r[t+1,:] = r[t,:] + v[t+1,:]*dt
@@ -94,3 +96,4 @@ plt.title('Displacement y: %.3f' % r[max_it,1])
 plt.show(True)
 
 
+#print v[max_it/6,:]
